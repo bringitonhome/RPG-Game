eeea4fe0bd1604dbcaca796a71f8b4e0c49234ac
Joel Therrien Vrignon <joel_t.v@hotmail.com>
2015-07-29 20:20:11 -0500

    Cleaned up main function
    Moved some actions into functions

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 using namespace std;
 
 int charSelect(sf::Event, int, Character*);
+sf::Texture loadTexture(string fileName);
+void showStats(Character *characters, string characterOrEnemy);
 
 int main()
 {
@@ -29,7 +31,7 @@ int main()
     int fastAlternate = 1;
     int halfAlternate = 1;
     int quarterAlternate = 1;
-    int selectedChar = 0;
+    int selectedChar = -1;
 
     srand(time(0));
 
@@ -38,31 +40,20 @@ int main()
     window.setPosition(sf::Vector2i(200, 0));
     window.setFramerateLimit(60);
 
-    //Setting up the background image
-    sf::Image bgImage;
-    if(!bgImage.loadFromFile("BG2.png")){
-    }
-    sf::Texture bgTexture;
-    if(!bgTexture.loadFromImage(bgImage)){
-    }
+    //Load in textures
+    sf::Texture textures[3] = {loadTexture("MasterSpriteSheet.png"), loadTexture("BG2.png"), loadTexture("Arrow 9x6.png")};
+
+    //Set Sprites
+
+    //Set up background
     sf::Sprite bgSprite;
-    bgSprite.setTexture(bgTexture);
-
-    //Loading Master Sprite sheet
-    sf::Image spriteSheet;
-    if(!spriteSheet.loadFromFile("MasterSpriteSheet.png")){
-    }
-    spriteSheet.createMaskFromColor(sf::Color::White);
-
-    sf::Texture charTexture;
-    if(!charTexture.loadFromImage(spriteSheet)){
-    }
+    bgSprite.setTexture(textures[1]);
 
     //Initializing Characters
     Character characters[NUMCHARACTERS];
 
     for(int x = 0; x < NUMCHARACTERS; x++){
-        characters[x].setSprite(&charTexture, x);
+        characters[x].setSprite(&textures[0], x);
         characters[x].setPosition(WW - 150, 75 + x*175);
     }
 
@@ -70,72 +61,40 @@ int main()
     Character enemies[NUMENEMIES];
 
     for(int x = 0; x < NUMENEMIES; x ++){
-        enemies[x].setSprite(&charTexture, x + 3);
+        enemies[x].setSprite(&textures[0], x + 3);
         enemies[x].setPosition(100, 75 + x*175);
     }
 
     //Inserting Arrow Image
-    sf::Image arrowImage;
-    if(!arrowImage.loadFromFile("Arrow 9x6.png")){
-    }
-    arrowImage.createMaskFromColor(sf::Color::White);
-
-    sf::Texture arrowTexture;
-    if(!arrowTexture.loadFromImage(arrowImage)){
-    }
-
     sf::Sprite arrow;
-    arrow.setTexture(arrowTexture);
+    arrow.setTexture(textures[2]);
     arrow.setScale(SCALE, SCALE);
 
 
     //Display stats
-    for(int x = 0; x < NUMCHARACTERS; x++){
-        cout << "Character " << x << ":" << endl << endl;
-        cout << "Attack: "  << characters[x].getAttribute(ATTACK)  << endl;
-        cout << "Defense: " << characters[x].getAttribute(DEFENSE) << endl;
-        cout << "Evade: "   << characters[x].getAttribute(EVADE)   << endl;
-        cout << "Luck: "    << characters[x].getAttribute(LUCK)    << endl;
-        cout << "Speed: "   << characters[x].getAttribute(SPEED)   << endl << endl;
-    }
-    for(int x = 0; x < NUMENEMIES; x++){
-        cout << "Enemy " << x << ":" << endl << endl;
-        cout << "Attack: "  << enemies[x].getAttribute(ATTACK)  << endl;
-        cout << "Defense: " << enemies[x].getAttribute(DEFENSE) << endl;
-        cout << "Evade: "   << enemies[x].getAttribute(EVADE)   << endl;
-        cout << "Luck: "    << enemies[x].getAttribute(LUCK)    << endl;
-        cout << "Speed: "   << enemies[x].getAttribute(SPEED)   << endl << endl;
-    }
-
+    showStats(characters, "Character");
+    showStats(enemies, "Enemy");
 
     while(window.isOpen())
     {
-
-
         sf::Event event;
         while(window.pollEvent(event)){
             if(event.type == sf::Event::Closed){
                 window.close();
             }
-
-
-
-                if(event.type == sf::Event::KeyReleased){
-                    characters[selectedChar].setSelected(false);
+                //if(event.type == sf::Event::KeyReleased){
                     selectedChar = charSelect(event, selectedChar, characters);
-                    characters[selectedChar].setSelected(true);
+
 
                     arrow.setPosition(characters[selectedChar].getSprite().getPosition().x,
                                       characters[selectedChar].getSprite().getPosition().y - 50);
                     arrowAlternate = 1;
-                }
 
+                //}
 
         }
 
         arrow.setPosition(characters[selectedChar].getSprite().getPosition().x, arrow.getPosition().y);
-
-
 
         //Animations
 
@@ -148,13 +107,17 @@ int main()
                 enemies[x].animateSprite(numLoop/(DELAY), x + 3, 6);
             }
 
+
+
             for(int x = 0; x < NUMCHARACTERS; x++){
                 characters[x].increaseATB();
+            }
+            for(int x = 0; x < NUMENEMIES; x++){
+                enemies[x].increaseATB();
             }
 
             fastAlternate *= -1;
         }
-
 
         //Half Speed
         if(numLoop % (DELAY*2) == 0){
@@ -162,10 +125,8 @@ int main()
             arrow.move(0, arrowAlternate*2*SCALE);
             arrowAlternate *= -1;
 
-
             halfAlternate *= -1;
         }
-
 
         //Quarter Speed
         if(numLoop % (DELAY*4) == 0){
@@ -193,41 +154,64 @@ int main()
         window.clear(sf::Color::Black);
         window.draw(bgSprite);
 
+
         window.draw(arrow);
+
 
         for(int x = 0; x < NUMCHARACTERS; x++){
             window.draw(characters[x].getSprite());
             window.draw(characters[x].getTimeBar());
             window.draw(characters[x].getTimeBarOutline());
         }
+
         for(int x = 0; x < NUMENEMIES; x++){
             window.draw(enemies[x].getSprite());
+            window.draw(enemies[x].getTimeBar());
+            window.draw(enemies[x].getTimeBarOutline());
         }
+
         window.display();
     }
 }
 
 int charSelect(sf::Event event, int selectedChar, Character *characters){
 
-    //bool upPressed = false;
-    //bool downPressed = false;
     int x = 0;
+    int readyCheck = 0;
 
-    if(event.key.code == sf::Keyboard::Up){
-        //upPressed = true;
-        selectedChar = (selectedChar - 1 + NUMCHARACTERS)%NUMCHARACTERS;
-        while(characters[selectedChar].isReady() == false && x < NUMCHARACTERS){
-            selectedChar = (selectedChar - 1 + NUMCHARACTERS)%NUMCHARACTERS;
-            x++;
-        }
+    for(int x = 0; x < NUMCHARACTERS; x++){
+        readyCheck += (int)characters[x].isReady();
     }
 
-    if(event.key.code == sf::Keyboard::Down){
-        //downPressed = true;
-        selectedChar = (selectedChar + 1 + NUMCHARACTERS)%NUMCHARACTERS;
-        while(characters[selectedChar].isReady() == false && x < NUMCHARACTERS){
+    if(readyCheck == 0){
+        selectedChar = -1;
+    }
+
+    if(readyCheck > 0){
+        characters[selectedChar].setSelected(false);
+        while(characters[selectedChar].isReady() == false){
+            selectedChar ++;
+        }
+        characters[selectedChar].setSelected(true);
+    }
+
+    if(event.type == sf::Event::KeyReleased){
+        if(event.key.code == sf::Keyboard::Up){
+            //upPressed = true;
+            selectedChar = (selectedChar - 1 + NUMCHARACTERS)%NUMCHARACTERS;
+            while(characters[selectedChar].isReady() == false && x < NUMCHARACTERS){
+                selectedChar = (selectedChar - 1 + NUMCHARACTERS)%NUMCHARACTERS;
+                x++;
+            }
+        }
+
+        if(event.key.code == sf::Keyboard::Down){
+            //downPressed = true;
             selectedChar = (selectedChar + 1 + NUMCHARACTERS)%NUMCHARACTERS;
-            x++;
+            while(characters[selectedChar].isReady() == false && x < NUMCHARACTERS){
+                selectedChar = (selectedChar + 1 + NUMCHARACTERS)%NUMCHARACTERS;
+                x++;
+            }
         }
     }
 
@@ -235,3 +219,31 @@ int charSelect(sf::Event event, int selectedChar, Character *characters){
 
     return selectedChar;
 }
+
+sf::Texture loadTexture(string fileName){
+    sf::Image image;
+    if(!image.loadFromFile(fileName)){
+    }
+    image.createMaskFromColor(sf::Color::White);
+    sf::Texture texture;
+    if(!texture.loadFromImage(image)){
+    }
+    return texture;
+}
+
+void showStats(Character *characters, string characterOrEnemy){
+
+    for(int x = 0; x < NUMCHARACTERS; x++){
+        cout << characterOrEnemy<< " " << x << ":" << endl << endl;
+        cout << "Attack: "  << characters[x].getAttribute(ATTACK)  << endl;
+        cout << "Defense: " << characters[x].getAttribute(DEFENSE) << endl;
+        cout << "Evade: "   << characters[x].getAttribute(EVADE)   << endl;
+        cout << "Luck: "    << characters[x].getAttribute(LUCK)    << endl;
+        cout << "Speed: "   << characters[x].getAttribute(SPEED)   << endl << endl;
+    }
+}
+
+
+
+
+
